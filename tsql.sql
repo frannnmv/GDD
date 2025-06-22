6c69611d2032fd90d7ac326d6b640c7320dc9d5d
Franco Moreno Vera <fmorenovera@frba.utn.edu.ar>
2025-06-22 16:10:19 -0300

    Ejercicio 3

--- tsql.sql
+++ tsql.sql
@@ -50,3 +50,23 @@ BEGIN
 	RETURN @stock - @ventas
 END
 GO
+
+/*
+3. Cree el/los objetos de base de datos necesarios para corregir la tabla empleado 
+en caso que sea necesario. Se sabe que debería existir un único gerente general 
+(debería ser el único empleado sin jefe). Si detecta que hay más de un empleado 
+sin jefe deberá elegir entre ellos el gerente general, el cual será seleccionado por 
+mayor salario. Si hay más de uno se seleccionara el de mayor antigüedad en la 
+empresa.  Al finalizar la ejecución del objeto la tabla deberá cumplir con la regla 
+de un único empleado sin jefe (el gerente general) y deberá retornar la cantidad 
+de empleados que había sin jefe antes de la ejecución. 
+*/
+
+CREATE PROCEDURE ejercicio3 @cantidad int output
+AS
+BEGIN
+	DECLARE @gerente_general numeric(6,0)
+	SELECT @cantidad = count(*) from Empleado where empl_jefe is null
+	SELECT top 1 @gerente_general = empl_codigo from Empleado where empl_jefe is null order by empl_salario desc, empl_ingreso
+	UPDATE Empleado set empl_jefe = @gerente_general where empl_jefe is null and empl_jefe <> @gerente_general
+END
