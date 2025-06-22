b0fde4938269e483c3e84763194da5a3e19b1387
Franco Moreno Vera <fmorenovera@frba.utn.edu.ar>
2025-06-22 15:34:42 -0300

    Ejercicio 2

--- tsql.sql
+++ tsql.sql
@@ -25,3 +25,28 @@ BEGIN
 	RETURN 'OCUPACION DEL DEPOSITO '+ @deposito+': ' + str((@stock/@stock_limite)*100)+'%'
 END
 GO
+
+/*
+2. Realizar una función que dado un artículo y una fecha, retorne el stock que 
+existía a esa fecha
+´*/
+
+CREATE FUNCTION ejercicio2 (@articulo char(8), @fecha smalldatetime)
+RETURNS decimal(12,2)
+AS
+BEGIN
+	DECLARE @stock decimal(12,2), @ventas decimal(12,2)
+	(
+		SELECT @stock = sum(isnull(stoc_cantidad,0)) from STOCK
+		where stoc_producto = @articulo
+		group by stoc_producto
+	)
+	(
+		SELECT @ventas = sum(item_cantidad) from Item_Factura 
+		join Factura on fact_tipo+fact_sucursal+fact_numero = item_tipo+item_sucursal+item_numero
+		where fact_fecha = @fecha and item_producto = @articulo
+		group by item_producto
+	)
+	RETURN @stock - @ventas
+END
+GO
